2058c6509c3006bea1e7eee040eaf74f4df24537
korolev_2512 <korolev-2512@mail.ru>
2024-05-07 20:18:34 +0500

    components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TelegramWebApp } from "@/providers/TelegramWebApp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TelegramWebApp>
+          {children}  
+        </TelegramWebApp>  
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,77 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        hugetitle: [
+          "64px",
+          {
+            lineHeight: "77px",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        largetitle: [
+          "32px",
+          {
+            lineHeight: "39px",
+            letterSpacing: "-0.02em",
+            fontWeight: "700",
+          },
+        ],
+        title: [
+          "24px",
+          {
+            lineHeight: "29px",
+            letterSpacing: "-0.02em",
+            fontWeight: "600",
+          },
+        ],
+        body: [
+          "20px",
+          {
+            lineHeight: "24px",
+            letterSpacing: "-0.02em",
+            fontWeight: "500",
+          },
+        ],
+        secondarybody: [
+          "16px",
+          {
+            lineHeight: "19px",
+            letterSpacing: "-0.02em",
+            fontWeight: "400",
+          },
+        ],
+      },
+      colors: {
+        TONColor: "#0098EA",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
