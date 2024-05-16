81cc78abacd46e63bc0c9ac16e3c51a8219c5162
korolev_2512 <korolev-2512@mail.ru>
2024-05-17 01:43:38 +0300

    added 404

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/src/app/styles/globals.css";
-import { TelegramWebApp } from "@/src/app/providers/TelegramWebApp";
+import "@/app/styles/globals.css";
+import { TelegramWebApp } from "@/app/providers/TelegramWebApp";
 
 const inter = Inter({ subsets: ["latin"] });
 
