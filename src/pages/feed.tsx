53fe050cb4025b7d0a6439fad8c1684e34c5bcd8
korolev_2512 <korolev-2512@mail.ru>
2024-06-02 03:15:32 +0300

    fixed feed

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -6,8 +6,12 @@ import Link from "next/link";
 export default function Feed() {
   return (
     <main className="bg-[#F2F2F7] flex flex-col gap-y-[18px] w-full items-center px-6 pt-4">
-      <Button className="w-full max-w-[500px]">Sign in to post</Button>
-      <TestFunction />
+        <Link href={"/addPost"}>
+            <Button className="w-full max-w-[500px]">
+                + Add new post
+            </Button>
+        </Link>
+        <TestFunction />
     </main>
   );
 }
