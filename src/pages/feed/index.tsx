cf89f58f8b31e29966ac2e2e4e0000f7403f5330
korolev_2512 <korolev-2512@mail.ru>
2024-06-08 18:09:43 +0300

    fixed addpost path

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Index() {
   return (
     <main className="bg-[#F2F2F7] flex flex-col gap-y-[18px] w-full items-center px-6 pt-4">
-        <Link href={"/addPost"} className="w-full max-w-[500px]">
+        <Link href={"/feed/addPost"} className="w-full max-w-[500px]">
             <Button className="w-full">
                 + Add new post
             </Button>
