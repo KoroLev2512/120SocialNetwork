2f279df4be3657090af5b031aa34ef4ef2826745
korolev_2512 <korolev-2512@mail.ru>
2024-06-16 21:16:21 +0500

    ability to add posts through the usage of localstorage, added back buttons

--- src/pages/feed/addPost/index.tsx
+++ src/pages/feed/addPost/index.tsx
@@ -1,22 +1,90 @@
-import React from "react";
+import { ChangeEvent, useState } from "react";
 import AddIcon from "@/shared/assets/icons/AddIcon";
-import {DefaultInput} from "@/shared/ui/input";
+import { DefaultInput } from "@/shared/ui/input";
+import BackButton from "@/shared/ui/backbutton";
+import { useRouter } from "next/router";
+import MainButton from "@/shared/ui/mainbutton";
+import Image from "next/image";
+import { Cross2Icon } from "@radix-ui/react-icons";
+import { InputTags } from "@/shared/ui/taginput";
+
+//todo: integrate shadcn forms
 
 export default function WelcomePage() {
+    const router = useRouter();
+    const [image, addImage] = useState<string | null>(null);
+    const [tags, setTags] = useState<string[]>([]);
+
+    const ImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (!file) return
+
+        const reader = new FileReader();
+    
+        reader.onload = (e) => {
+            const imageURL = e.target?.result as string | null;
+            addImage(imageURL);
+        };
+        reader.readAsDataURL(file);
+    };
+
+    const removeImage = () => {
+        addImage(null);
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const post = {
+            tags,
+            image
+        };
+
+        const allPosts = JSON.parse(localStorage.getItem('posts') || '[]');
+        allPosts.push(post);
+        localStorage.setItem('posts', JSON.stringify(allPosts));
+        router.push("/feed")
+    }
+
     return (
         <main className="flex h-screen w-full flex-col items-center bg-app_gray_light-100 dark:bg-app_gray_dark-300">
+            <BackButton onClick={() => router.back()} />
             <div className="flex flex-col items-center py-[24px]">
                 <h1 className="text-title font-bold">Add up to 3 photos</h1>
             </div>
-            <div className="flex w-full flex-col bg-gray-300 rounded-[14px] max-w-[350px] min-h-[350px]">
-                <div className="inline-flex w-full h-full items-center justify-center px-[40px] py-[14px]">
-                    <AddIcon/>
+            <form className="w-full relative items-center flex flex-col" onSubmit={handleSubmit}>
+                <div className="relative flex mx-auto justify-center w-full">
+                {image ? (
+                    <div className="relative">
+                        <Image className="object-cover aspect-square max-w-[350px] rounded-[14px]" width={512} height={512} src={image} alt="test" />
+                        <button type="button" onClick={removeImage} className="absolute bg-white top-3 right-3 rounded-full p-1.5">
+                            <Cross2Icon className="text-app_gray_light-300" />
+                        </button>
+                    </div>
+                ) : (
+                    <label htmlFor="imageUpload" className="cursor-pointer w-full mx-auto max-w-[350px] min-h-[350px] flex flex-col">
+                        <div className="flex w-full flex-col bg-app_gray_light-200 dark:bg-app_gray_dark-100 rounded-[14px] max-w-[350px] min-h-[350px]">
+                            <div className="inline-flex w-full h-full items-center justify-center px-[40px] py-[14px]">
+                                <AddIcon />
+                                <input type="file" id="imageUpload" onChange={ImageUpload} accept="image/*" style={{ display: "none" }} />
+                            </div>
+                        </div>
+                    </label>
+                )}
                 </div>
-            </div>
-            <div className="w-full flex flex-col gap-y-4 mt-6">
-                <DefaultInput placeholder="Blah blah blah" label="Description"/>
-                <DefaultInput placeholder="https://" className="placeholder:text-app_blue" label="URL to the post"/>
-            </div>
+                <div className="w-full flex flex-col gap-y-4 mt-6">
+                    <InputTags
+                        label="Description"
+                        max={3}
+                        value={tags}
+                        onChange={setTags}
+                        placeholder="Add a tag"
+                    />
+                    <DefaultInput placeholder="https://" className="text-app_blue" label="URL to the post" />
+                </div>
+                <MainButton onClick={() => handleSubmit} text="Publish" color="#0098EA" />
+                <button type="submit">test</button>
+            </form>
         </main>
     );
 };
---
+++ src/shared/ui/taginput.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import * as React from "react";
+import { Badge } from "@/shared/ui/badge";
+import { Button } from "./button";
+import { cn } from "@/app/lib/utils";
+import { type InputProps } from "./input";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+
+type InputTagsProps = Omit<InputProps, "value" | "onChange"> & {
+  value: string[];
+  onChange: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+const InputTags = React.forwardRef<HTMLInputElement, InputTagsProps>(
+  ({ className, value, onChange, ...props }, ref) => {
+    const [pendingDataPoint, setPendingDataPoint] = React.useState("");
+
+    React.useEffect(() => {
+      if (pendingDataPoint.includes(",")) {
+        const newDataPoints = new Set([
+          ...value,
+          ...pendingDataPoint.split(",").map((chunk) => chunk.trim()),
+        ]);
+        onChange(Array.from(newDataPoints));
+        setPendingDataPoint("");
+      }
+    }, [pendingDataPoint, onChange, value]);
+
+    const addPendingDataPoint = () => {
+      if (pendingDataPoint) {
+        const newDataPoints = new Set([...value, pendingDataPoint]);
+        onChange(Array.from(newDataPoints));
+        setPendingDataPoint("");
+      }
+    };
+
+    return (
+      <div
+        className={cn(
+          "has-[:focus-visible]:outline-none py-3 relative px-6 gap-x-2 inline-flex w-full bg-white dark:bg-app_gray_dark-200 placeholder:text-app_gray_light-300/60 duration-300 transition-all focus-visible:outline-none focus-visible:ring-none disabled:cursor-not-allowed disabled:opacity-50",
+          className, props.label && ("pt-[30px]")
+        )}
+      >
+        {value.map((item) => (
+          <Badge key={item}>
+            {item}
+            <button
+              onClick={() => {
+                onChange(value.filter((i) => i !== item));
+              }}
+            >
+              <XMarkIcon className="size-4" />
+            </button>
+          </Badge>
+        ))}
+        {props.label && (
+            <label className="absolute left-6 top-3 text-[14px] tracking-[-0.04em] select-none leading-[18px] text-app_gray_light-300/60">
+              {props.label}
+            </label>
+        )}
+        <input
+          className="flex-1 outline-none placeholder:text-app_gray_light-300/60 bg-white dark:bg-app_gray_dark-200 space-x-2 max-w-[72px]"
+          value={pendingDataPoint}
+          onChange={(e) => setPendingDataPoint(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === ",") {
+              e.preventDefault();
+              addPendingDataPoint();
+            } else if (
+              e.key === "Backspace" &&
+              pendingDataPoint.length === 0 &&
+              value.length > 0
+            ) {
+              e.preventDefault();
+              onChange(value.slice(0, -1));
+            }
+          }}
+          {...props}
+          ref={ref}
+        />
+      </div>
+    );
+  }
+);
+
+InputTags.displayName = "InputTags";
+
+export { InputTags };
