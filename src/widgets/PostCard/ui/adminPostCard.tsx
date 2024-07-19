8480ff00625051ab5e42265e55b63d55e1284bb5
korolev_2512 <korolev-2512@mail.ru>
2024-07-20 00:49:28 +0500

    fixed postCard bug

--- src/widgets/PostCard/ui/adminPostCard.tsx
+++ src/widgets/PostCard/ui/adminPostCard.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from "axios";
+import { AvatarImage, Avatar, AvatarFallback } from "@/shared/ui/avatar";
+import { AvatarIcon } from "@radix-ui/react-icons";
+import Image, { StaticImageData } from "next/image";
 
 type postCardProps = {
     post_id: number;
@@ -52,11 +55,15 @@ const AdminPostCard: React.FC<postCardProps> = ({
     return (
         <div className="max-w-[389px] w-full rounded-[14px] bg-white flex flex-col gap-y-0.5">
             <div className="inline-flex items-center gap-x-1.5 py-1 pl-1">
-                <img
-                    src={usersAvatar}
-                    className="size-[38px] rounded-full"
-                    alt={'avatar'}
-                />
+                <Avatar className="size-[38px]">
+                    {usersAvatar ? (
+                        <AvatarImage width={128} height={128} alt="Avatar" src={usersAvatar} />
+                    ) : (
+                        <AvatarFallback>
+                            <AvatarIcon className="text-black/40" height={38} width={38} />
+                        </AvatarFallback>
+                    )}
+                </Avatar>
                 <div className="flex flex-col">
                     <p className="text-secondarybody text-black">{user}</p>
                     <p className="text-[12px] tracking-[-0.02em] text-app_gray_light-300">
