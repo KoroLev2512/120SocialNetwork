9907bb8d6a545d2449dfef67e9cbebb68b3e891a
korolev_2512 <korolev-2512@mail.ru>
2024-07-22 21:51:35 +0500

    added language support & ran prettier in OrientationWarning

--- src/widgets/OrientationWarning/index.tsx
+++ src/widgets/OrientationWarning/index.tsx
@@ -1,7 +1,10 @@
-import TiltMeIcon from '@/shared/icons/TiltMeIcon';
-import { useEffect, useState } from 'react';
+import TiltMeIcon from "@/shared/icons/TiltMeIcon";
+import { GetStaticPropsContext } from "next";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
 
 const OrientationWarning = () => {
+  const t = useTranslations("orientationWarning");
   const [isLandscape, setLandscape] = useState(false);
 
   useEffect(() => {
@@ -13,30 +16,38 @@ const OrientationWarning = () => {
       }
     };
 
-    window.addEventListener('resize', OrientationChange);
+    window.addEventListener("resize", OrientationChange);
     OrientationChange();
 
     return () => {
-      window.removeEventListener('resize', OrientationChange);
+      window.removeEventListener("resize", OrientationChange);
     };
   }, []);
 
   return (
     <>
       {isLandscape && (
-        <div className='z-[999] fixed h-screen w-full bg-app_gray_light-100 dark:bg-app_gray_dark-300 justify-center flex flex-col items-center gap-y-[16px]'>
-            <TiltMeIcon className='text-[#007AFF] size-[140px]' />
-            <div className='flex flex-col items-center gap-y-[5px]'>
-                <h1 className='text-[32px] font-bold'>Переверни меня</h1>
-                <p className='text-[20px] text-center text-black/40 dark:text-white/40 font-medium'>
-                Приложение 120Block работает <br />
-                только в вертикальном режиме 
-                </p>
-            </div>
+        <div className="fixed z-[999] flex h-screen w-full flex-col items-center justify-center gap-y-[16px] bg-app_gray_light-100 dark:bg-app_gray_dark-300">
+          <TiltMeIcon className="size-[140px] text-[#007AFF]" />
+          <div className="flex flex-col items-center gap-y-[5px]">
+            <h1 className="text-[32px] font-bold">{t("title")}</h1>
+            <p className="max-w-[318px] text-center text-[20px] font-medium leading-[24px] text-black/40 dark:text-white/40">
+              {t("text")}
+            </p>
+          </div>
         </div>
       )}
     </>
   );
 };
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: (await import(`../../../languages_test/${locale}.json`))
+        .default,
+    },
+  };
+}
 
 export default OrientationWarning;
